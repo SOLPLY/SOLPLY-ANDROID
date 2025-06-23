c72aab66cd44ed11f74d2e8caafeb82476abc856
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2025-06-23 22:15:15 +0900

    feature#3 add oauth feature module

---
+++ feature/oauth/src/main/java/com/teamsolply/solply/oauth/presentation/OauthScreen.kt
@@ -0,0 +1,32 @@
+package com.teamsolply.solply.oauth.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun OauthRoute(
+    paddingValues: PaddingValues
+) {
+    OauthScreen()
+}
+
+@Composable
+fun OauthScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "Oauth"
+        )
+    }
+}
