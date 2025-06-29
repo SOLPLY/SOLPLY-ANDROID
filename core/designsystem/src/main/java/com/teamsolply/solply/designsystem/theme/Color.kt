95b0ecfd267e915627c42e9a11218fe16c7ff075
88guri <quffl2002@naver.com>
2025-06-29 18:06:25 +0900

    feature: Add Color Theme

--- core/designsystem/src/main/java/com/teamsolply/solply/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/teamsolply/solply/designsystem/theme/Color.kt
@@ -2,5 +2,59 @@ package com.teamsolply.solply.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Grayscale
 val White = Color(0xFFFFFFFF)
-val Black = Color(0xFF000000)
+val Gray100 = Color(0xFFF7F5F6)
+val Gray200 = Color(0xFFEEEAEC)
+val Gray300 = Color(0xFFE3DDE0)
+val Gray400 = Color(0xFFD1CACD)
+val Gray500 = Color(0xFFB8AFB3)
+val Gray600 = Color(0xFFA89DA0)
+val Gray700 = Color(0xFF888183)
+val Gray800 = Color(0xFF696666)
+val Gray900 = Color(0xFF4F4B4B)
+val Black = Color(0xFF242424)
+
+// Red
+val Red100 = Color(0xFFFFF2EB)
+val Red200 = Color(0xFFFFE1D3)
+val Red300 = Color(0xFFF7CFBF)
+val Red400 = Color(0xFFF1A699)
+val Red500 = Color(0xFFEA867B)
+val Red600 = Color(0xFFE46A61)
+val Red700 = Color(0xFFC94F44)
+val Red800 = Color(0xFFAA473E)
+val Red900 = Color(0xFF823E32)
+
+// Yellow
+val Yellow100 = Color(0xFFFFF3DA)
+val Yellow200 = Color(0xFFFFE6B0)
+val Yellow300 = Color(0xFFF8D17C)
+val Yellow400 = Color(0xFFF9C45C)
+val Yellow500 = Color(0xFFF5B640)
+val Yellow600 = Color(0xFFD8982A)
+val Yellow700 = Color(0xFFA8772E)
+val Yellow800 = Color(0xFF66513F)
+val Yellow900 = Color(0xFF4E433B)
+
+// Green
+val Green100 = Color(0xFFFAFBEC)
+val Green200 = Color(0xFFF8FCD9)
+val Green300 = Color(0xFFE9F0AF)
+val Green400 = Color(0xFFD3DD91)
+val Green500 = Color(0xFFACBD6F)
+val Green600 = Color(0xFF94AB65)
+val Green700 = Color(0xFF7F9360)
+val Green800 = Color(0xFF626F4F)
+val Green900 = Color(0xFF4E5940)
+
+// Purple
+val Purple100 = Color(0xFFF6F6FF)
+val Purple200 = Color(0xFFE8E8FF)
+val Purple300 = Color(0xFFDDDDF5)
+val Purple400 = Color(0xFFCBCBE7)
+val Purple500 = Color(0xFFB4B2DB)
+val Purple600 = Color(0xFF9796C7)
+val Purple700 = Color(0xFF7B7AAD)
+val Purple800 = Color(0xFF595880)
+val Purple900 = Color(0xFF33324D)
