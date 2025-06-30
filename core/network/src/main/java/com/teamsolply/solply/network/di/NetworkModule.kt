7dde6e59cb5e9b71f33a1f8e849309585ef9967f
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2025-07-01 03:47:37 +0900

    feature#22 di setting

--- core/network/src/main/java/com/teamsolply/solply/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/teamsolply/solply/network/di/NetworkModule.kt
@@ -1,14 +1,18 @@
 package com.teamsolply.solply.network.di
 
+import android.content.Context
 import android.util.Log
 import androidx.datastore.core.DataStore
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.teamsolply.solply.common.buildconfig.BuildConfigFieldProvider
 import com.teamsolply.solply.datastore.SolplyTokenData
 import com.teamsolply.solply.network.AccessTokenInterceptor
+import com.teamsolply.solply.network.TokenRefreshAuthenticator
+import com.teamsolply.solply.network.service.TokenRefreshService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
 import okhttp3.Interceptor
@@ -65,10 +69,19 @@ object NetworkModule {
         }
     }
 
+    @Provides
+    @Singleton
+    fun provideTokenRefreshAuthenticator(
+        dataStore: DataStore<SolplyTokenData>,
+        tokenRefreshService: TokenRefreshService,
+        @ApplicationContext context: Context
+    ): TokenRefreshAuthenticator = TokenRefreshAuthenticator(dataStore, tokenRefreshService, context)
+
     private fun createOkHttpClient(
         loggingInterceptor: HttpLoggingInterceptor,
         errorInterceptor: Interceptor,
-        accessTokenInterceptor: AccessTokenInterceptor? = null
+        accessTokenInterceptor: AccessTokenInterceptor? = null,
+        tokenRefreshAuthenticator: TokenRefreshAuthenticator? = null
     ): OkHttpClient =
         OkHttpClient.Builder()
             .retryOnConnectionFailure(true)
@@ -76,6 +89,7 @@ object NetworkModule {
             .addInterceptor(errorInterceptor)
             .apply {
                 accessTokenInterceptor?.let { addInterceptor(it) }
+                tokenRefreshAuthenticator?.let { authenticator(it) }
             }
             .connectTimeout(30, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
@@ -88,8 +102,14 @@ object NetworkModule {
     fun provideAuthOkHttpClient(
         loggingInterceptor: HttpLoggingInterceptor,
         errorInterceptor: Interceptor,
-        accessTokenInterceptor: AccessTokenInterceptor
-    ): OkHttpClient = createOkHttpClient(loggingInterceptor, errorInterceptor, accessTokenInterceptor)
+        accessTokenInterceptor: AccessTokenInterceptor,
+        tokenRefreshAuthenticator: TokenRefreshAuthenticator
+    ): OkHttpClient = createOkHttpClient(
+        loggingInterceptor,
+        errorInterceptor,
+        accessTokenInterceptor,
+        tokenRefreshAuthenticator
+    )
 
     @Provides
     @Singleton
