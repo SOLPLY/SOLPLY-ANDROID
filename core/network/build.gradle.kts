994b82c3cde3a0c924711af5caa2d9f011de22d2
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2025-07-01 03:47:27 +0900

    feature#22 refresh token authenticator

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -18,4 +18,5 @@ dependencies {
     implementation(libs.retrofit.core)
     implementation(libs.retrofit.kotlin.serialization)
     implementation(libs.okhttp.logging)
+    implementation(libs.process.phoenix)
 }
