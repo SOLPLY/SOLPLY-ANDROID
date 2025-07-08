9331fbc055ba7f2cd755dfc8155df5850ff80f3a
88guri <quffl2002@naver.com>
2025-07-09 02:10:43 +0900

    feature#38 RoundedCornerShape

--- feature/onboarding/src/main/java/com/teamsolply/solply/onboarding/component/ProgressBar.kt
+++ feature/onboarding/src/main/java/com/teamsolply/solply/onboarding/component/ProgressBar.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -17,6 +18,7 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.teamsolply.solply.designsystem.theme.SolplyTheme
@@ -46,19 +48,56 @@ fun ProgressBar(
             .fillMaxWidth()
             .height(8.dp)
             .padding(horizontal = 16.dp)
+            .clip(RoundedCornerShape(20.dp))
             .background(SolplyTheme.colors.gray300)
     ) {
         Box(
             modifier = Modifier
                 .fillMaxWidth(size)
                 .height(8.dp)
-                .background(SolplyTheme.colors.gray900)
+                .background(SolplyTheme.colors.gray900, shape = RoundedCornerShape(20.dp))
+                .clip(RoundedCornerShape(20.dp))
                 .animateContentSize()
         )
     }
 }
 
+@Composable
+fun PreviewableProgressBar(progress: Float) {
+    val size by animateFloatAsState(
+        targetValue = progress,
+        animationSpec = tween(
+            durationMillis = 1000,
+            delayMillis = 200,
+            easing = LinearOutSlowInEasing,
+        ),
+        label = "animation",
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(8.dp)
+            .padding(horizontal = 16.dp)
+            .clip(RoundedCornerShape(20.dp))
+            .background(SolplyTheme.colors.gray300)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(size)
+                .height(8.dp)
+                .background(SolplyTheme.colors.gray900, shape = RoundedCornerShape(20.dp))
+                .clip(RoundedCornerShape(20.dp))
+                .animateContentSize()
+        )
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun ProgressBarPreview() {
+    SolplyTheme {
+        PreviewableProgressBar(progress = (2f / 3f) * 0.95f) // 약 63%
+    }
 }
