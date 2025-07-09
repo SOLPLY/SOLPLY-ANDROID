f4a4c0622205eeb95a7167780b3818ec1f89703f
ImHyungsuk <limhs8630@gmail.com>
2025-07-09 13:15:53 +0900

    feature#36 Test tabrow & horizontalpager

--- feature/mypage/src/main/java/com/teamsolply/solply/mypage/MypageScreen.kt
+++ feature/mypage/src/main/java/com/teamsolply/solply/mypage/MypageScreen.kt
@@ -11,10 +11,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SecondaryTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.teamsolply.solply.designsystem.component.button.SolplyBasicButton
 import com.teamsolply.solply.designsystem.theme.SolplyTheme
@@ -22,6 +32,7 @@ import com.teamsolply.solply.model.MapsType
 import com.teamsolply.solply.mypage.component.MypageTopBar
 import com.teamsolply.solply.ui.extension.customClickable
 import com.teamsolply.solply.ui.preview.DefaultPreview
+import kotlinx.coroutines.launch
 
 @Composable
 fun MypageRoute(
@@ -33,12 +44,16 @@ fun MypageRoute(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MypageScreen(
     navigateToMaps: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val coroutineScope = rememberCoroutineScope()
     val pagerState = rememberPagerState(pageCount = { 2 })
+    val selectedIndex = pagerState.currentPage
+    val list = listOf("장소", "코스")
     Column(
         modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
@@ -48,17 +63,41 @@ fun MypageScreen(
             title = "수집함",
             onBackButtonClick = {}
         )
+        TabRow(
+            selectedTabIndex = 0,
+            modifier = Modifier.fillMaxWidth(),
+            indicator = { tabPositions ->
+                TabRowDefaults.SecondaryIndicator(
+                    modifier = Modifier.tabIndicatorOffset(tabPositions[selectedIndex]),
+                    color = SolplyTheme.colors.black
+                )
+            }
+        ) {
+            list.forEachIndexed { index, page ->
+                val selected = pagerState.currentPage == index
+                Tab(
+                    selected = selected,
+                    onClick = { coroutineScope.launch { pagerState.animateScrollToPage(index) } },
+                    modifier = Modifier.padding(vertical = 14.dp)
+                ) {
+                    Text(
+                        text = page,
+                        style = SolplyTheme.typography.head15Sb,
+                        color = SolplyTheme.colors.black
+                    )
+                }
+
+            }
+        }
         HorizontalPager(
-            modifier = Modifier
-                .fillMaxSize(),
+            modifier = Modifier,
             state = pagerState
         ) { page ->
-            // Our page content
-            Text(
-                text = "Page: $page",
-                modifier = Modifier.fillMaxWidth()
-            )
-
+            when (page) {
+                0 -> PlaceCollectionScreen(
+                    onClickEmptyButton = {}
+                )
+            }
         }
         Text(
             text = "Mypage",
