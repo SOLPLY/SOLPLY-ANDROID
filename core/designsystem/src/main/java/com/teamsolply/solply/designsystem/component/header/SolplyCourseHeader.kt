c52034b1da61fbf98f943e8a263ffa9c3dc58533
88guri <quffl2002@naver.com>
2025-07-10 05:51:57 +0900

    feature#41 Line organize

--- core/designsystem/src/main/java/com/teamsolply/solply/designsystem/component/header/SolplyCourseHeader.kt
+++ core/designsystem/src/main/java/com/teamsolply/solply/designsystem/component/header/SolplyCourseHeader.kt
@@ -28,15 +28,13 @@ fun CourseHeader(
         "EXPLORER" -> "골목 곳곳을 탐색하는\n${nickname}님을 위한 오늘의 코스"
         "MOODING" -> "취향을 모으는\n${nickname}님을 위한 오늘의 코스"
         "NATURAL" -> "힐링이 필요한\n${nickname}님을 위한 오늘의 코스"
-        else
-            ->"솔플리가 추천하는\n${nickname}님을 위한 오늘의 코스"
+        else ->"솔플리가 추천하는\n${nickname}님을 위한 오늘의 코스"
     }
 
     Column(
         modifier = modifier
             .fillMaxWidth()
             .padding(top = 16.dp),
-
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         Row(
@@ -45,54 +43,41 @@ fun CourseHeader(
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Row(verticalAlignment = Alignment.CenterVertically) {
-
                 Icon(
                     painter = painterResource(id = com.teamsolply.solply.designsystem.R.drawable.ic_home_location),
                     contentDescription = "town-icon",
-
                     modifier = Modifier
                         .padding(1.dp)
                         .size(20.dp)
                 )
-
                 Text(
                     text = townName,
                     style = SolplyTheme.typography.body16M,
-
                     modifier = Modifier
                         .padding(horizontal = 4.dp)
                         .height(21.dp)
                 )
-
                 Icon(
                     painter = painterResource(id = com.teamsolply.solply.designsystem.R.drawable.ic_arrow_right_icon),
                     contentDescription = "arrow-right-icon",
-
                     modifier = Modifier
                         .padding(vertical = 7.dp)
                         .size(24.dp)
                 )
-
             }
-
             Icon(
                 painter = painterResource(id = com.teamsolply.solply.designsystem.R.drawable.ic_setting_icon),
                 contentDescription = "setting-icon",
-
                 modifier = Modifier
                     .padding(1.dp)
                     .size(24.dp)
             )
-
         }
-
         Text(
             text = recommendText,
             style = SolplyTheme.typography.display20Sb,
-
             modifier = Modifier
                 .padding(start = 4.dp, top = 16.dp, bottom = 9.dp)
         )
-
     }
 }
