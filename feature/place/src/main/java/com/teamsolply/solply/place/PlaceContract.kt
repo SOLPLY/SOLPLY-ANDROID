24762384257f44ac14597a08ad9d74e126011e95
나갱 <helen030816@naver.com>
2025-07-10 13:45:36 +0900

    feature#43 place screen 스크롤 불가능 영역 ui 완성

--- feature/place/src/main/java/com/teamsolply/solply/place/PlaceContract.kt
+++ feature/place/src/main/java/com/teamsolply/solply/place/PlaceContract.kt
@@ -1,30 +1,37 @@
 package com.teamsolply.solply.place
 
-import com.teamsolply.solply.designsystem.component.chip.PlaceTag
 import com.teamsolply.solply.model.PlaceType
-import com.teamsolply.solply.place.model.PlaceInfo
+import com.teamsolply.solply.place.model.PlaceUser
+import com.teamsolply.solply.place.model.RecommendPlaceInfo
 import com.teamsolply.solply.ui.base.SideEffect
 import com.teamsolply.solply.ui.base.UiIntent
 import com.teamsolply.solply.ui.base.UiState
 import okhttp3.internal.immutableListOf
 
 data class PlaceState(
-    val places: List<PlaceInfo> = immutableListOf(
-        PlaceInfo(
+    val user: PlaceUser = PlaceUser(
+        userId = 0,
+        nickname = "숭이",
+        favoriteTowns = "연희동",
+        persona = "HEALING",
+    ),
+
+    val recommendplaces: List<RecommendPlaceInfo> = immutableListOf(
+        RecommendPlaceInfo(
             placeId = 0,
             placeName = "장소 이름",
             thumbnailImageUrl = com.teamsolply.solply.designsystem.R.drawable.img_course_dummy,
             primaryTag = PlaceType.CAFE,
             description = "장소 한 줄 소개 장소 한 줄 소개"
         ),
-        PlaceInfo(
+        RecommendPlaceInfo(
             placeId = 1,
             placeName = "장소 이름",
             thumbnailImageUrl = com.teamsolply.solply.designsystem.R.drawable.img_course_dummy,
             primaryTag = PlaceType.FOOD,
             description = "장소 한 줄 소개 장소 한 줄 소개"
         ),
-        PlaceInfo(
+        RecommendPlaceInfo(
             placeId = 2,
             placeName = "장소 이름",
             thumbnailImageUrl = com.teamsolply.solply.designsystem.R.drawable.img_course_dummy,
@@ -32,6 +39,7 @@ data class PlaceState(
             description = "장소 한 줄 소개 장소 한 줄 소개 두 줄이 되어도 괜찮음음음음음음"
         ),
     )
+
 ) : UiState
 
 sealed interface PlaceIntent : UiIntent {
--- feature/place/src/main/java/com/teamsolply/solply/place/PlaceScreen.kt
+++ feature/place/src/main/java/com/teamsolply/solply/place/PlaceScreen.kt
@@ -1,19 +1,19 @@
 package com.teamsolply.solply.place
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -23,9 +23,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
-import com.teamsolply.solply.place.component.PlaceRecommendCard
+import com.teamsolply.solply.designsystem.component.header.PlaceHeader
+import com.teamsolply.solply.place.component.card.PlaceRecommendCard
 import com.teamsolply.solply.ui.lifecycle.LaunchedEffectWithLifecycle
 import kotlinx.coroutines.flow.collectLatest
 
@@ -62,39 +61,51 @@ fun PlaceScreen(
     val sideItemSize = 180.dp
 
     val pagerState = rememberPagerState(
-        initialPage = Int.MAX_VALUE / 2 - ((Int.MAX_VALUE / 2) % state.places.size),
+        initialPage = Int.MAX_VALUE / 2 - ((Int.MAX_VALUE / 2) % state.recommendplaces.size),
         pageCount = { Int.MAX_VALUE }
     )
 
     val horizontalPadding = (screenWidth - centerItemSize) / 2 + (centerItemSize - sideItemSize) / 2
+    Column(modifier = modifier.fillMaxSize()) {
+        PlaceHeader(
+            townName = state.user.favoriteTowns,
+            persona = state.user.persona,
+            nickname = state.user.nickname,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(28.dp))
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        HorizontalPager(
-            state = pagerState,
-            pageSpacing = 46.dp,
+        Box(
             modifier = Modifier
-                .height(centerItemSize)
-                .fillMaxWidth(),
-            contentPadding = PaddingValues(horizontal = horizontalPadding)
-        ) { page ->
-            val place = state.places[page % state.places.size]
-            val isCenter = page == pagerState.currentPage
-
-            PlaceRecommendCard(
-                title = place.placeName,
-                subtitle = place.description,
-                type = place.primaryTag,
-                imgRes = place.thumbnailImageUrl,
+                .fillMaxSize()
+        ) {
+            HorizontalPager(
+                state = pagerState,
+                pageSpacing = 46.dp,
                 modifier = Modifier
-                    .requiredWidth(if (isCenter) centerItemSize else sideItemSize)
-                    .requiredHeight(if (isCenter) centerItemSize else sideItemSize),
-                onClick = { onPlaceClick(place.placeId) }
+                    .height(centerItemSize)
+                    .fillMaxWidth(),
+                contentPadding = PaddingValues(horizontal = horizontalPadding)
+            ) { page ->
+                val place = state.recommendplaces[page % state.recommendplaces.size]
+                val isCenter = page == pagerState.currentPage
+
+                PlaceRecommendCard(
+                    title = place.placeName,
+                    subtitle = place.description,
+                    type = place.primaryTag,
+                    imgRes = place.thumbnailImageUrl,
+                    modifier = Modifier
+                        .requiredWidth(if (isCenter) centerItemSize else sideItemSize)
+                        .requiredHeight(if (isCenter) centerItemSize else sideItemSize),
+                    onClick = { onPlaceClick(place.placeId) }
+                )
+
+            }
+            SnackbarHost(
+                hostState = snackbarHostState,
+                modifier = Modifier.align(Alignment.BottomCenter)
             )
         }
-
-        SnackbarHost(hostState = snackbarHostState, modifier = Modifier.align(Alignment.BottomCenter))
     }
 }
