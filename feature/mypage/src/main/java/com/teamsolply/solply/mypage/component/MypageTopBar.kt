e35a58613ee05e94a7cc756e3ac9cc3ad0dc3955
ImHyungsuk <limhs8630@gmail.com>
2025-07-12 23:27:02 +0900

    chore#36 apply ktlint

--- feature/mypage/src/main/java/com/teamsolply/solply/mypage/component/MypageTopBar.kt
+++ feature/mypage/src/main/java/com/teamsolply/solply/mypage/component/MypageTopBar.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.teamsolply.solply.designsystem.theme.SolplyTheme
@@ -25,7 +24,7 @@ import com.teamsolply.solply.ui.extension.customClickable
 fun MypageTopBar(
     barText: String,
     onBackButtonClick: () -> Unit,
-    isTownSelected: Boolean,
+    isTownSelected: Boolean
 ) {
     Row(
         modifier = Modifier
