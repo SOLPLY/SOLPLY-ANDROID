61fb14ab80867d9e9acfc1feab7873da2d61943c
88guri <quffl2002@naver.com>
2025-07-15 18:15:10 +0900

    feature#75 Make SplashScreen

--- feature/main/src/main/java/com/teamsolply/solply/main/splash/SplashScreen.kt
+++ feature/main/src/main/java/com/teamsolply/solply/main/splash/SplashScreen.kt
@@ -1,15 +1,21 @@
 package com.teamsolply.solply.main.splash
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.flow.collectLatest
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.*
+import com.teamsolply.solply.designsystem.theme.SolplyTheme
+import com.teamsolply.solply.main.R
+import androidx.compose.foundation.layout.size
 
 @Composable
 fun SplashScreen(
@@ -27,13 +33,22 @@ fun SplashScreen(
         }
     }
 
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.splash))
+    val progress by animateLottieCompositionAsState(composition)
+
     Column(
-        modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = SolplyTheme.colors.green300),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Splash"
+        LottieAnimation(
+            composition = composition,
+            progress = { progress },
+            modifier = Modifier
+                .size(122.dp)
         )
     }
 }
+
