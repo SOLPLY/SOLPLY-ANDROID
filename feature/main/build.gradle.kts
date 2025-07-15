72eeb7ac7b9bfef7cb84929c5df1eff95254ff06
88guri <quffl2002@naver.com>
2025-07-15 18:13:51 +0900

    feature#75 Add dependencies

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -14,4 +14,6 @@ dependencies {
     implementation(projects.feature.course)
     implementation(projects.feature.maps)
     implementation(projects.feature.mypage)
+    implementation("androidx.core:core-splashscreen:1.0.0")
+    implementation ("com.airbnb.android:lottie-compose:6.1.0")
 }
