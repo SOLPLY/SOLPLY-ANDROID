bc04b08351b16faecbfec257d8213792a6e5b70e
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2025-07-17 02:48:25 +0900

    refactor#81 select town button

--- feature/onboarding/src/main/java/com/teamsolply/solply/onboarding/screen/SelectTownScreen.kt
+++ feature/onboarding/src/main/java/com/teamsolply/solply/onboarding/screen/SelectTownScreen.kt
@@ -4,12 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.teamsolply.solply.designsystem.component.button.AddLocalAreaButton
@@ -48,7 +48,10 @@ fun SelectTownScreen(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 28.dp),
-                horizontalArrangement = Arrangement.spacedBy(12.dp)
+                horizontalArrangement = Arrangement.spacedBy(
+                    space = 12.dp,
+                    alignment = Alignment.CenterHorizontally
+                )
             ) {
                 val towns = townList.favoriteTownList
                 if (towns.isEmpty()) {
@@ -62,17 +65,13 @@ fun SelectTownScreen(
                             selected = state.selectedTownId == subTown.townId
                         )
                     }
+                    AddLocalAreaButton(
+                        text = "",
+                        onClick = { },
+                        isShowMore = true,
+                        selected = false,
+                    )
                 }
-
-                AddLocalAreaButton(
-                    text = "",
-                    onClick = { },
-                    isShowMore = true,
-                    selected = false,
-                    modifier = Modifier
-                        .weight(1f)
-                        .aspectRatio(1f)
-                )
             }
         }
 
