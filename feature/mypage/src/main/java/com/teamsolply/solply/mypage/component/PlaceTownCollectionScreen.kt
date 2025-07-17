4b0c41ab1ed1aed069cc483bb7355d89bc2b281f
ImHyungsuk <limhs8630@gmail.com>
2025-07-18 01:43:52 +0900

    refactor#105 fix mypage place thumbnail

--- feature/mypage/src/main/java/com/teamsolply/solply/mypage/component/PlaceTownCollectionScreen.kt
+++ feature/mypage/src/main/java/com/teamsolply/solply/mypage/component/PlaceTownCollectionScreen.kt
@@ -9,13 +9,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.teamsolply.solply.mypage.model.PlaceTownEntity
 import com.teamsolply.solply.ui.extension.customClickable
+import com.teamsolply.solply.ui.image.AdaptationImage
 
 @Composable
 fun PlaceTownCollectionScreen(
@@ -55,9 +59,14 @@ fun PlaceTownCollectionScreen(
                         Modifier.padding(start = 5.dp)
                     },
                     content = {
-                        Image(
-                            painter = painterResource(com.teamsolply.solply.designsystem.R.drawable.img_course_dummy),
-                            contentDescription = ""
+                        AdaptationImage(
+                            imageUrl = it.imageUrl,
+                            modifier = Modifier
+                                .matchParentSize()
+                                .clip(
+                                    RoundedCornerShape(20.dp)
+                                ),
+                            contentScale = ContentScale.Crop
                         )
                     }
                 )
