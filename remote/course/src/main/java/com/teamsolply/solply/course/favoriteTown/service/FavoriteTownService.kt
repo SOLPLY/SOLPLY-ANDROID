b4fcbcf94b27fc3cbff96acc6484b638567b5b22
88guri <quffl2002@naver.com>
2025-09-18 00:03:17 +0900

    feature#129 change api

--- remote/course/src/main/java/com/teamsolply/solply/course/favoriteTown/service/FavoriteTownService.kt
+++ remote/course/src/main/java/com/teamsolply/solply/course/favoriteTown/service/FavoriteTownService.kt
@@ -1,16 +1,15 @@
 package com.teamsolply.solply.course.favoriteTown.service
 
-import com.teamsolply.solply.course.favoriteTown.dto.FavoriteTownUserInfoResponseDto
 import com.teamsolply.solply.course.favoriteTown.dto.PatchUserFavTownRequestDto
+import com.teamsolply.solply.course.favoriteTown.dto.TownTreeResponseDto
 import com.teamsolply.solply.network.model.BaseResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.PATCH
 
 interface FavoriteTownService {
-    @GET("/api/users/towns")
-    suspend fun getTownList(): BaseResponse<FavoriteTownUserInfoResponseDto>
-
+    @GET("/api/towns")
+    suspend fun getTownTree(): BaseResponse<TownTreeResponseDto>
     @PATCH("/api/users/towns")
     suspend fun patchUserFavoriteTown(
         @Body body: PatchUserFavTownRequestDto
