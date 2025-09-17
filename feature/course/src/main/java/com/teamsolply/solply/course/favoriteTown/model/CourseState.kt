01f8d2e76564decb0d57b5aba8a6e465bbc1c7c6
88guri <quffl2002@naver.com>
2025-09-18 00:04:23 +0900

    feature#129 add CourseState

--- feature/course/src/main/java/com/teamsolply/solply/course/favoriteTown/model/CourseState.kt
+++ feature/course/src/main/java/com/teamsolply/solply/course/favoriteTown/model/CourseState.kt
@@ -1,6 +1,11 @@
 package com.teamsolply.solply.course.favoriteTown.model
 
+import com.teamsolply.solply.ui.base.UiState
+
 data class CourseState(
-    val selectedTownId: Long?,
-    val townList: List<TownModel>
-)
+    val selectedTownId: Long? = null,
+    val selectedRegionId: Long? = null,
+    val regions: List<Region> = emptyList(),
+    val townsByRegion: Map<Long, List<TownLite>> = emptyMap(),
+    val townInfo: FavoriteTownInfoEntity? = null
+) : UiState
