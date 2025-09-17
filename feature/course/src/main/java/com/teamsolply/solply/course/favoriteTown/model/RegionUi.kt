38b039a8590b8ea69a42157e0fbe8237833a141c
88guri <quffl2002@naver.com>
2025-09-17 14:21:07 +0900

    feature#129 delete RegionUi TownModel

--- feature/course/src/main/java/com/teamsolply/solply/course/favoriteTown/model/RegionUi.kt
+++
@@ -1,6 +0,0 @@
-package com.teamsolply.solply.course.favoriteTown.model
-
-data class RegionUi(
-    val id: Long,
-    val name: String
-)
--- feature/course/src/main/java/com/teamsolply/solply/course/favoriteTown/model/TownModel.kt
+++
@@ -1,6 +0,0 @@
-package com.teamsolply.solply.course.favoriteTown.model
-
-data class TownModel(
-    val id: Long,
-    val name: String
-)
