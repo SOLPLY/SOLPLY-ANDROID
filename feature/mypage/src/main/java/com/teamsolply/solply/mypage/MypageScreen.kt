4b6b6ae4995189efb7e418daa2b1adcd6fe44984
ImHyungsuk <limhs8630@gmail.com>
2025-09-24 16:45:41 +0900

    feature#132 프로필 수정 화면 배치

--- feature/mypage/src/main/java/com/teamsolply/solply/mypage/MypageScreen.kt
+++ feature/mypage/src/main/java/com/teamsolply/solply/mypage/MypageScreen.kt
@@ -3,7 +3,6 @@ package com.teamsolply.solply.mypage
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -20,8 +19,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
---
+++ feature/mypage/src/main/java/com/teamsolply/solply/mypage/profile/ProfileScreen.kt
@@ -0,0 +1,127 @@
+package com.teamsolply.solply.mypage.profile
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.teamsolply.solply.designsystem.component.button.SolplyBasicButton
+import com.teamsolply.solply.designsystem.component.textfield.SolplyNicknameTextField
+import com.teamsolply.solply.designsystem.component.topbar.SolplyTopBar
+import com.teamsolply.solply.designsystem.theme.SolplyTheme
+import com.teamsolply.solply.mypage.R
+
+@Composable
+fun ProfileScreen(
+    onBackButtonClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = SolplyTheme.colors.white),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SolplyTopBar(
+            barText = stringResource(R.string.profile_edit),
+            onBackButtonClick = onBackButtonClick,
+        )
+        Spacer(
+            modifier = Modifier.weight(1f)
+        )
+        Image(
+            painter = painterResource(R.drawable.img_basic_profile),
+            contentDescription = stringResource(R.string.profile_img),
+            modifier = Modifier
+                .width(92.dp)
+                .height(92.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Fit
+        )
+        Spacer(modifier = Modifier.weight(2f))
+        Column(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = stringResource(R.string.profile_nickname),
+                    color = SolplyTheme.colors.black,
+                    style = SolplyTheme.typography.body16M
+                )
+            }
+            SolplyNicknameTextField(
+                value = "",
+                isNicknameDuplicate = false,
+                onValueChange = {},
+                checkNicknameValidate = { true },
+                changeNicknameValidate = {},
+                modifier = Modifier.padding(top = 12.dp)
+            )
+        }
+        Spacer(modifier = Modifier.weight(1.5f))
+        Column(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = stringResource(R.string.profile_nickname),
+                    color = SolplyTheme.colors.black,
+                    style = SolplyTheme.typography.body16M
+                )
+            }
+            SolplyNicknameTextField(
+                value = "",
+                isNicknameDuplicate = false,
+                onValueChange = {},
+                checkNicknameValidate = { true },
+                changeNicknameValidate = {},
+                modifier = Modifier.padding(vertical = 12.dp)
+            )
+        }
+        Spacer(modifier = Modifier.weight(12f))
+        SolplyBasicButton(
+            text = "완료",
+            onClick = {},
+            modifier = Modifier.padding(vertical = 24.dp, horizontal = 16.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ProfileScreenPreview() {
+    SolplyTheme {
+        ProfileScreen(onBackButtonClick = {})
+
+    }
+}
